89fd5793cbee2f0489a8b998f7b794ab5cb2e7f8
MoeshaHlongwane <nikiwehlongwane22@gmail.com>
2024-12-11 13:51:52 +0200

    src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> ||
-    <router-link to="/dashboard">Employees dashBoard</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/dashboard">
+        
+          <h4>  <img src="./assets/tech-solution-logo.jpg" alt="Company Logo" width="50" />Modern Tech-Solutions</h4>
+          <span><h5>Dashboard</h5></span>
+        </router-link>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" />
+          <button class="btn btn-outline-primary" type="button">Search</button>
+        </form>
+        <button class="btn btn-danger ms-3" @click="showLogoutDialog = true">Logout</button>
+      </div>
+    </nav>
+    <LogoutDialog v-if="showLogoutDialog" @confirm="logout" @cancel="showLogoutDialog = false" />
+    <router-view />
+  </div>
 </template>
 
+<script>
+import LogoutDialog from "./components/LogoutDialog.vue";
+
+export default {
+  data() {
+    return {
+      showLogoutDialog: false,
+    };
+  },
+  methods: {
+    logout() {
+      this.showLogoutDialog = false;
+      this.$router.push("/"); // Redirect to Home on logout
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
+.navbar-brand span {
   font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin-left: 10px;
 }
 </style>
+
