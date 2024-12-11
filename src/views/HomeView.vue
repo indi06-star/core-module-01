156e85bc34554e5959dae3d9b11e97e5d0a50739
MoeshaHlongwane <nikiwehlongwane22@gmail.com>
2024-12-11 13:57:41 +0200

    src/views/HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <NavBar/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import NavBar from '@/components/NavBar.vue';
 
 export default {
+  components: {
+    NavBar
+  }
  
 }
 </script>
