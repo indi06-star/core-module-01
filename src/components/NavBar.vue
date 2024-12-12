01cd324d4ede534f4fd16f47d85fad2202c899a1
MoeshaHlongwane <nikiwehlongwane22@gmail.com>
2024-12-12 09:41:50 +0200

    src/components/NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,58 +1,29 @@
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <router-link class="navbar-brand" to="/">
-          <img src="/logo.png" alt="Company Logo" width="50" />
-          <span>Welcome to HR Dashboard</span>
-        </router-link>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" />
-          <button class="btn btn-outline-primary" type="button">Search</button>
-        </form>
-        <button class="btn btn-danger ms-3" @click="showLogoutDialog = true">Logout</button>
-      </div>
-    </nav>
-    <LogoutDialog v-if="showLogoutDialog" @confirm="logout" @cancel="showLogoutDialog = false" />
-    <router-view />
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Modern Tech</a>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+      <button class="btn btn-danger ms-3" @click="logout">Logout</button>
+    </div>
+  </nav>
 </template>
 
 <script>
-import LogoutDialog from "./components/LogoutDialog.vue";
-
 export default {
-  data() {
-    return {
-      showLogoutDialog: false,
-    };
-  },
   methods: {
     logout() {
-      this.showLogoutDialog = false;
-      this.$router.push("/"); // Redirect to Home on logout
+      if (confirm("Are you sure you want to log out?")) {
+        alert("Logged out");
+      }
     },
   },
 };
 </script>
 
 <style>
-.navbar-brand span {
-  font-weight: bold;
-  margin-left: 10px;
-}
-</style> -->
-<template >
-  <div>
-    <h1>hi</h1>
-    
-  </div>
-</template>
-<script>
-export default {
-  
-}
-</script>
-<style>
-  
+/* Add custom styles */
 </style>
+
