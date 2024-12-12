3583c5ebc87e44ff9efd4afecaa0974a2075ee2b
MoeshaHlongwane <nikiwehlongwane22@gmail.com>
2024-12-12 09:41:10 +0200

    src/components/Modal.vue

---
+++ src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="modal d-block" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">{{ employee.name }}</h5>
+            <button type="button" class="btn-close" @click="$emit('close')"></button>
+          </div>
+          <div class="modal-body">
+            <p>Position: {{ employee.position }}</p>
+            <p>Department: {{ employee.department }}</p>
+            <p>Contact: {{ employee.contact }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ["employee"],
+  };
+  </script>
+  
+  <style>
+  .modal {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+  </style>
+  
