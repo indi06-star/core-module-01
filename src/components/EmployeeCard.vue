943fd63c0f866dceb8268b7ad5c06392cc6d1bdc
MoeshaHlongwane <nikiwehlongwane22@gmail.com>
2024-12-12 15:32:14 +0200

    src/components/EmployeeCard.vue

--- src/components/EmployeeCard.vue
+++ src/components/EmployeeCard.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="col-md-4 mb-3">
       <div class="card">
-        <img :src="employee.image" class="card-img-top" alt="Employee Image" />
+        <img
+          :src="employee.gender === 'female' ? './assets/female-icon.png' : './assets/male-icon.png'"
+          class="card-img-top"
+          alt="Employee Icon"
+        />
         <div class="card-body">
           <h5 class="card-title">{{ employee.name }}</h5>
-          <p class="card-text">{{ employee.position }}</p>
-          <p class="card-text"><small class="text-muted">{{ employee.department }}</small></p>
-          <button @click="$emit('view-details', employee)" class="btn btn-primary">Show More</button>
+          <p class="card-text">{{ employee.position }} - {{ employee.department }}</p>
+          <button class="btn btn-primary" @click="showDetails">Show More</button>
         </div>
       </div>
     </div>
@@ -15,22 +18,13 @@
   <script>
   export default {
     props: {
-      employee: {
-        type: Object,
-        required: true,
+      employee: Object,
+    },
+    methods: {
+      showDetails() {
+        this.$emit("showDetails", this.employee);
       },
     },
   };
   </script>
-  
-  <style scoped>
-  .card {
-    text-align: center;
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  }
-  .card-img-top {
-    height: 150px;
-    object-fit: cover;
-  }
-  </style>
   
