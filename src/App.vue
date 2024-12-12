efac84825ec0ca34abbe5c489669f08d579aea1a
MoeshaHlongwane <nikiwehlongwane22@gmail.com>
2024-12-12 09:36:29 +0200

    src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,18 +21,13 @@
 </template>
 
 <script>
-import HeroSection from "./components/HeroSection.vue";
+
 import LogoutDialog from "./components/LogoutDialog.vue";
 
 export default {
-<<<<<<< HEAD
-  components :{
-    LogoutDialog
-=======
   components:{
-    HeroSection
-
->>>>>>> 1cb46550cb240cda41abcfa67c5273fb3765b063
+    
+    LogoutDialog
   },
   data() {
     return {
